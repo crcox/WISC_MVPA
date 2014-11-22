32026d7694dee83a0ee8b46aa492b6a0a2facff0
Chris Cox <crcox@wisc.edu>
2014-11-22 15:42:12 -0600

    added testdata

---
+++ testdata/remake_test_data.m
@@ -0,0 +1,33 @@
+%% Setup metadata for demo:
+nSubjects = 10;
+metadata(10).TrueFaces = [];
+metadata(10).xyz_tlrc = [];
+metadata(10).CVBLOCKS = [];
+for ss = 1:nSubjects
+	disp(ss)
+	metadata(ss).CVBLOCKS = bsxfun(@eq,reshape(mod(0:999,10),100,10),0:9);
+		metadata(ss).TrueFaces = [true(50,1);false(50,1)];
+	while true
+			metadata(ss).xyz_tlrc = unique([randsample(8,500,true),randsample(8,500,true),randsample(8,500,true)],'rows');
+		if length(metadata(ss).xyz_tlrc)>250
+			metadata(ss).xyz_tlrc = sortrows(metadata(ss).xyz_tlrc(randperm(length(metadata(ss).xyz_tlrc),250),:));
+			break
+		end
+	end
+end
+save('testdata/metadata.mat','metadata');
+
+%% Set up X
+% Here is where you would load your data.
+X = cell(nSubjects,1);
+actVox = false(250,10);
+for ss = 1:nSubjects
+	s.X = randn(100,250);
+	metadata(ss).actVox = pdist2([4.5,4.5,4.5],metadata(ss).xyz_tlrc)<2;
+	s.X(Y{ss},metadata(ss).actVox) = s.X(Y{ss},metadata(ss).actVox) + 3;
+	X{ss} = bsxfun(@minus,s.X,mean(s.X));
+	X{ss} = bsxfun(@rdivide,X{ss},std(X{ss}));
+end
+clear s;
+save('testdata/alldata.mat', 'X');
+save(
