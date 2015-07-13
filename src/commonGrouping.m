70427966c5feda7f0745676f8a1e214de0d9527d
ChrisCox <crcox@wisc.edu>
2015-07-12 20:42:41 -0400

    Handle empty cells.

--- src/commonGrouping.m
+++ src/commonGrouping.m
@@ -1,13 +1,13 @@
 function [Gc,ix] = commonGrouping(G)
     n = uint32(max(cellfun('length', G),[],2));
     c = uint32([0;cumsum(n(:))]);
-    N = uint32(max(cellfun(@max, G)));
+    N = cellmax(G);
     for i = 1:size(G,1)
-        for j = 1:size(G,2)
-            x = uint32(G{i,j});
-            g = uint32([x; ones(n(i)-length(x),1,'uint32')*(N(j)+1)]);
-            G{i,j} = g;
-        end
+      for j = 1:size(G,2)
+        x = uint32(G{i,j});
+        g = uint32([x; ones(n(i)-length(x),1,'uint32')*(N(j)+1)]);
+        G{i,j} = g;
+      end
     end
     ix = cell2mat(G);
     
@@ -18,23 +18,8 @@ function [Gc,ix] = commonGrouping(G)
         Gc{i} = uint32(a:b);
     end
 end
-    
-%     N = max(cellfun(@max, G));
-%     for i = 1:size(G,1)
-%         for j = 1:size(G,2)
-%             x = G{i,j};
-%             G{i,j} = [x; ones(n(i)-length(x),1)*(N(j)+1)];
-%         end
-%     end
-%     
-%     ix = cell2mat(G);
-%     z = false(size(ix));
-%     for i = 1:size(ix, 2)
-%         x = ix(:,i);
-%         [~,ai] = unique(x,'stable');
-%         z(ai,i) = true;
-%         z(x==(N(i)+1),i) = true;
-%     end
-%     sum(z)
-%     ix = reshape(ix(z), [], size(ix, 2)); 
-    
+
+function N = cellmax(C)
+  C(cellfun('isempty',C)) = {uint32(0)}; %handle empty cells
+  N = uint32(max(cellfun(@max, C)));
+end
