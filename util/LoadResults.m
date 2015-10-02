19ad8149569ac67471ddcd098902a5bf2157b7bb
Chris Cox <cox.crc@gmail.com>
2015-10-01 22:55:53 -0500

    preparing utils/LoadResults for SOSLasso

--- util/LoadResults.m
+++ util/LoadResults.m
@@ -1,26 +1,34 @@
 function [results, params] = LoadResults(varargin)
   p = inputParser();
   addParameter(p,'ResultDir','.',@ischar);
-  addParameter(p,'DataDir','/media/chris/FacePlaceObject/SOSLASSO/SOSLasso/batch-002/tuning', @ischar)
+  addParameter(p,'DataDir','~/MCW/WholeBrain_RSA/data/avg',@ischar)
+  addParameter(p,'orientation','mni')
+  addParameter(p,'CVSchemesFile','CV_schemes.mat',@ischar)
+  addParameter(p,'cv_var','CV',@ischar)
   addParameter(p,'MetadataFile','metadata.mat',@ischar)
-  addParameter(p,'Filters',{'rowfilter'},@iscell)
+  addParameter(p,'meta_var','metadata',@ischar)
+  addParameter(p,'Filters',{'TrueFaces'},@iscell)
+  addParameter(p,'ResultFile','',@ischar);
+  addParameter(p,'ParamFile','',@ischar);
   addParameter(p,'SortJobs',false,@islogical)
   addParameter(p,'SkipFields',[])
-  addParameter(p,'ResultFile','fitObj.mat',@ischar);
-  addParameter(p,'ParamFile','params.json',@ischar);
   parse(p,varargin{:});
 
-  resultdir  = p.Results.ResultDir;
-  datadir    = p.Results.DataDir;
-  metafile   = p.Results.MetadataFile;
-  filters    = p.Results.Filters;
-  sortjobs   = p.Results.SortJobs;
-  SKIP       = p.Results.SkipFields;
-  resultfile = p.Results.ResultFile;
-  paramfile  = p.Results.ParamFile;
-  allfiles   = dir(resultdir);
-  alldirs    = allfiles([allfiles.isdir]);
-  jobdirs    = SelectJobDirs(alldirs, paramfile, sortjobs);
+  resultdir     = p.Results.ResultDir;
+  datadir       = p.Results.DataDir;
+  orientation   = p.Results.orientation;;
+  cvsfile       = p.Results.CVSchemesFile;
+  cv_var        = p.Results.cv_var;
+  metafile      = p.Results.MetadataFile;
+  meta_varname  = p.Results.meta_var;
+  filter_labels = p.Results.Filters;
+  sortjobs      = p.Results.SortJobs;
+  resultfile    = p.Results.ResultFile;
+  paramfile     = p.Results.ParamFile;
+  SKIP          = p.Results.SkipFields;
+  allfiles      = dir(resultdir);
+  alldirs       = allfiles([allfiles.isdir]);
+  jobdirs       = SelectJobDirs(alldirs, 'root',resultdir, 'sort', sortjobs);
 
   if ~isempty(SKIP)
     SkipStr = SKIP;
@@ -29,107 +37,109 @@ function [results, params] = LoadResults(varargin)
     fprintf('Fields %s will be skipped.\n', SkipStr);
   end
 
-  metapath = fullfile(datadir,metafile);
-  load(metapath, 'metadata');
-  cvscheme = 1;
+  cvspath = fullfile(datadir,cvsfile);
+  StagingContainer = load(cvspath, cv_varname);
+  CV = StagingContainer.(cv_varname);
+
+  metapath = fullfile(datadir, metafile);
+  StagingContainer = load(metapath, meta_varname);
+  metadata = StagingContainer.(meta_varname);
 
   n = length(jobdirs);
   nchar = 0;
   fprintf('Loading job ');
-  ind = 0;
   for i = 1:n;
     fprintf(repmat('\b', 1, nchar));
     nchar = fprintf('%d of %d', i, n);
 
-    jobdir      = jobdirs(i).name;
+    % load parameter file
+    jobdir      = fullfile(resultdir, jobdirs(i).name);
     parampath   = fullfile(jobdir,paramfile);
     tmp         = loadjson(parampath);
     tmp.jobdir  = jobdir;
     params(i)   = tmp;
+    clear tmp;
 
-    %subjectNumbers = cellfun(@(x) sscanf(x, 'jlp%02d.mat'), params.data);
-    subjectNumbers = 1:10;
-    includedSubjects = ismember([metadata.subject], subjectNumbers);
-    metadata = metadata(includedSubjects);
-    [~,sidx] = ismember(subjectNumbers, [metadata.subject]);
+    % Pull cv indexes
+    cvind       = params(i).cvholdout;
+    finalind    = params(i).finalholdout;
+    cvscheme    = params(i).cvscheme;
 
-    cvIndexes = 1:size(metadata(1).CVBLOCKS,2);
-    finalind = params.FinalHoldoutSet;
-    cvIndexes(finalind) = [];
-
-    % These will be used when looping over the cv and subject elements of the
-    % fitObj.
-    [snum, cvind] = ndgrid(subjectNumbers,cvIndexes);
-
-    filter = cell(length(subjectNumbers),1);
-    cvfilter = cell(length(subjectNumbers),length(cvIndexes));
-    finalfilter = cell(length(subjectNumbers),length(cvIndexes));
-    for ss = 1:length(subjectNumbers);
-      for ii = 1:length(filters);
-        fname = filters{ii};
-        z = metadata(sidx(ss)).(fname);
-        if ii == 1;
-          filter{ss} = z;
-        else
-          filter{ss} = filter{ss} & z;
-        end
-      end
-      for cc = 1:length(cvIndexes)
-        cvfilter{ss,cc} = metadata(ss).CVBLOCKS(filter{ss},cvIndexes(cc));
-        finalfilter{ss,cc} = metadata(ss).CVBLOCKS(filter{ss},finalind);
-      end
-    end
-%    cvind       = params(i).cvholdout;
-%    finalind    = params(i).finalholdout;
-%    cvscheme    = params(i).cvscheme;
-
+    % load results file
     resultpath = fullfile(jobdir, resultfile);
-    if exist(resultpath, 'file')
-      load(resultpath, 'fitObj');
-      N = numel(fitObj);
-      for ii = 1:N
-        train = ~cvfilter{ii}(~finalfilter{ii});
-        test = cvfilter{ii}(~finalfilter{ii});
-        ind = ind + 1;
-        tmp             = fitObj(ii);
-        tmp.dp1         = computeStat(fitObj(ii), test, 'dprime');
-        tmp.dp2         = computeStat(fitObj(ii), train, 'dprime');
-        tmp.err1        = computeStat(fitObj(ii), test, 'error');
-        tmp.err2        = computeStat(fitObj(ii), train, 'error');
-        tmp.nz_rows     = computeStat(fitObj(ii), [], 'nzvox');
-        tmp.cvfilter    = cvfilter;
-        tmp.finalfilter = finalfilter;
-        tmp.pind        = i;
-        tmp.cvholdout   = cvind(ii);
-        tmp.subject     = snum(ii);
-        if ~isempty(SKIP)
-          nskip = length(SKIP);
-          for iii = 1:nskip
-            tmp = rmfield(tmp, SKIP{iii});
-          end
-        end
-        results(ind)  = tmp;
+    if ~exist(resultpath, 'file')
+      continue;
+    end
+    R = load(resultpath);
+
+
+    % Construct filters
+    if isempty(filter_labels)
+      rowfilter = true(1, nrow);
+      colfilter = true(1, ncol);
+    else
+      if ~iscell(filter_labels);
+        filter_labels = {filter_labels};
+      end
+      % M.filter points to a structured array of filters.
+      % First, force filters to a common orientation.
+      for ii = 1:numel(M.filter)
+        M.filter(ii).filter = forceRowVec(M.filter(ii).filter);
+      end
+      % Then select the filters
+      z = false(1,numel(M.filter));
+      for f = filter_labels;
+        z(strcmp(f, {M.filter.label})) = true;
+      end
+      z = z & strcmp(data_varname, {M.filter.subset});
+
+      filters.row = M.filter(z & [M.filter.dimension]==1);
+      filters.col = M.filter(z & [M.filter.dimension]==2);
+      if isempty(filters.row)
+        rowfilter = true(1, nrow);
+      else
+        rowfilter = all(cat(1, filters.row.filter),1);
+      end
+      if isempty(filters.col)
+        colfilter = true(1, ncol);
+      else
+        colfilter = all(cat(1, filters.col.filter),1);
+      end
+      clear filters;
+    end
+    cvfilter    = cvind == CV(rowfilter,cvscheme);
+    finalfilter = finalind == CV(rowfilter,cvscheme);
+
+    % apply filters
+    S = SS(rowfilter,rowfilter);
+    R.S = S(~finalfilter,~finalfilter);
+    R.S_test = S(cvfilter,cvfilter);
+    XYZ = XYZ(colfilter,:);
+
+    % log coordinates of selected voxels
+    R.coords.label = coord_varname;
+    R.coords.xyz = XYZ(R.nz_rows,:);
+
+    % log metadata
+    R.cvind = cvind;
+    R.cvfilter = cvfilter;
+    R.finalfilter = finalfilter;
+
+    % Drop any variables that should not be held in memory
+    if ~isempty(SKIP)
+      nskip = length(SKIP);
+      for ii = 1:nskip
+        R = rmfield(R, SKIP{ii});
       end
     end
+
+    results(i) = R;
+
   end
-  fprintf(' %d\n', ind)
+  fprintf('\n')
 end
 
-function x = computeStat(fitObj, filter, stat)
-  y  = fitObj.Y(filter) > 0;
-  yz = fitObj.Yh(filter) > 0;
-  switch stat
-  case 'error'
-    x = 1-(sum(y==yz)/length(y));
-  case 'dprime'
-    ntp = sum(y);
-    nfp = sum(~y);
-    x = (sum(y&yz)/ntp) - (sum(~y&yz)/nfp);
-  case 'nzvox'
-    x = sum(fitObj.betas~=0);
-  end
-end
-
+%% Private Functions
 function y = selectcv(x,cv)
   if numel(x)==1
     if iscell(x);
@@ -155,18 +165,29 @@ function y = selectcv(x,cv)
     end
   end
 end
-function jobdirs = SelectJobDirs(dirs,paramfile, SORT)
+
+function jobdirs = SelectJobDirs(dirs,varargin)
+  p = inputParser;
+  addRequired(p, 'dirs');
+  addParameter(p, 'root','.', @ischar);
+  addParameter(p, 'sort',false, @islogical);
+  parse(p, dirs, varargin{:});
+  
+  dirs = p.Results.dirs;
+  SORT = p.Results.sort;
+  root = p.Results.root;
+  
   N = length(dirs);
   isJobDir = false(N,1);
   for ii = 1:N
-    jobdir = dirs(ii).name;
+    jobdir = fullfile(root,dirs(ii).name);
     % Check if a special dir (current or parent)
     if any(strcmp(jobdir,{'.','..'}))
       continue
     end
     % Check if contains parameter file.
-    parampath = fullfile(jobdir, paramfile);
-    if exist(parampath, 'file')
+    paramfile = fullfile(jobdir, 'params.json');
+    if exist(paramfile, 'file')
       isJobDir(ii) = true;
     end
   end
@@ -184,4 +205,12 @@ function jobdirs = SelectJobDirs(dirs,paramfile, SORT)
     jobdirs = jobdirs(ix);
   end
   fprintf('Found %d job directories.\n', length(jobdirs))
+end
+
+function r = forceRowVec(v)
+  r = v(1:end);
+end
+
+function c = forceColVec(v)
+  c = v(:);
 end
