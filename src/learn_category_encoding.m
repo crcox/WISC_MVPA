2afd3d4360b3ca6c4a3b8597561a41f79eef61d3
Chris Cox <crcox@wisc.edu>
2015-10-22 21:11:02 -0500

    changed the way results are stored for soslasso

--- src/learn_category_encoding.m
+++ src/learn_category_encoding.m
@@ -58,43 +58,51 @@ function [results,info] = learn_category_encoding(Y, X, Gtype, varargin)
     cvset = holdout;
   end
 
-  zz = zeros(ncv,nalpha,nlam);
-  h1 = cell(size(X)); [h1{:}] = deal(zz);
-  h2 = cell(size(X)); [h2{:}] = deal(zz);
-  f1 = cell(size(X)); [f1{:}] = deal(zz);
-  f2 = cell(size(X)); [f2{:}] = deal(zz);
-  m1 = cell(size(X)); [m1{:}] = deal(zz);
-  m2 = cell(size(X)); [m2{:}] = deal(zz);
-  c1 = cell(size(X)); [c1{:}] = deal(zz);
-  c2 = cell(size(X)); [c2{:}] = deal(zz);
-  err1 = cell(size(X)); [err1{:}] = deal(zz);
-  err2 = cell(size(X)); [err2{:}] = deal(zz);
-  nt1 = cell(size(X)); [err2{:}] = deal(zz);
-  nt2 = cell(size(X)); [err2{:}] = deal(zz);
-  nd1 = cell(size(X)); [err2{:}] = deal(zz);
-  nd2 = cell(size(X)); [err2{:}] = deal(zz);
-  clear zz;
-
-  cc = cell(ncv,nalpha,nlam);
-  WzAll   = cell(size(X)); [WzAll{:}] = deal(cc);
-  YzAll   = cell(size(X)); [YzAll{:}] = deal(cc);
-  clear cc;
-
   % Set number of tasks (i.e., subjects)
   if iscell(X)
-    t = size(X,1);
+    t = numel(X);
   else
     t = 1;
   end
 
+  % Define results struct
+  results.Wz = [];
+  results.Wix = [];
+  results.Yz = [];
+  results.Wnz = [];
+  results.nvox = [];
+  results.subject = [];
+  results.cvholdout = [];
+  results.finalholdout = [];
+  results.alpha = [];
+  results.lambda = [];
+  results.diameter = [];
+  results.overlap = [];
+  results.shape = [];
+  results.nt1  = [];
+  results.nt2  = [];
+  results.nd1  = [];
+  results.nd2  = [];
+  results.h1   = [];
+  results.h2   = [];
+  results.f1   = [];
+  results.f2   = [];
+  results.err1 = [];
+  results.err2 = [];
+
+  % Preallocate
+  results(t*ncv*nlam*nalpha).Wz = [];
+
   fprintf('%7s%5s %7s %7s %11s %11s %11s %11s %11s\n', '','subj','alpha','lambda','test err','train err','test diff','train diff','n vox')
-	for i = cvset
+  iii = 0;
+  for i = 1:ncv
+    icv = cvset(i);
     X = Xorig;
 
     test_set = cell(size(cvind));
     train_set = cell(size(cvind));
     for ii = 1:numel(cvind)
-      test_set{ii}  = cvind{ii}==i;
+      test_set{ii}  = cvind{ii}==icv;
       train_set{ii} = ~test_set{ii};
     end
 
@@ -167,8 +175,8 @@ function [results,info] = learn_category_encoding(Y, X, Gtype, varargin)
               z = Wz{ii} ~= 0;
               if nnz(z) > 0
                 cv = cvind{ii};
-                cv = cv(cv~=i);
-                cv(cv>i) = cv(cv>i) - 1;
+                cv = cv(cv~=icv);
+                cv(cv>icv) = cv(cv>icv) - 1;
 
                 debiasObj = cvglmnet(X{ii}(train_set{ii},z),Y{ii}(train_set{ii}), ...
                            'binomial',opts_debias,'class',max(cv),cv);
@@ -179,21 +187,7 @@ function [results,info] = learn_category_encoding(Y, X, Gtype, varargin)
             end
           end
         end
-        for ii = 1:numel(X);
-          WzAll{ii}{i,j,k} = Wz{ii};
-          YzAll{ii}{i,j,k} = X{ii}*Wz{ii};
-          Yz = YzAll{ii}{i,j,k};
-          h1{ii}(i,j,k)   = nnz( Y{ii}(test_set{ii})>0  & (Yz(test_set{ii})>0)  );
-          h2{ii}(i,j,k)   = nnz( Y{ii}(train_set{ii})>0 & (Yz(train_set{ii})>0) );
-          f1{ii}(i,j,k)   = nnz(~Y{ii}(test_set{ii})>0  & (Yz(test_set{ii})>0)  );
-          f2{ii}(i,j,k)   = nnz(~Y{ii}(train_set{ii})>0 & (Yz(train_set{ii})>0) );
-          err1{ii}(i,j,k) = nnz( Y{ii}(test_set{ii})>0  ~= (Yz(test_set{ii})>0 ));
-          err2{ii}(i,j,k) = nnz( Y{ii}(train_set{ii})>0 ~= (Yz(train_set{ii})>0));
-          nt1{ii}(i,j,k)  = nnz(Y{ii}(test_set{ii})>0);
-          nt2{ii}(i,j,k)  = nnz(Y{ii}(train_set{ii})>0);
-          nd1{ii}(i,j,k)  = nnz(Y{ii}(test_set{ii})<=0);
-          nd2{ii}(i,j,k)  = nnz(Y{ii}(train_set{ii})<=0);
-        end
+
         k1 = sum(cellfun(@nnz,Wz))/t;
 
         if isempty(ALPHA)
@@ -208,71 +202,60 @@ function [results,info] = learn_category_encoding(Y, X, Gtype, varargin)
           lambda_k = LAMBDA(k);
         end
 
-        for ii = 1:numel(X)
-          fprintf('cv %3d: %3d |%6.2f |%6.2f |%10d |%10d |%10d |%10d |%10d |\n', ...
-            i,ii,alpha_j,lambda_k,err1{ii}(i,j,k),err2{ii}(i,j,k),h1{ii}(i,j,k)-f1{ii}(i,j,k),h2{ii}(i,j,k)-f2{ii}(i,j,k),nnz(WzAll{ii}{i,j,k}));
-        end
-
-%        fprintf('Exit status -- %s (%d iterations)\n', info.message, info.iter);
-      end % lamba loop
-    end % alpha loop
-	end % cv loop
-  iii = 0;
-  for ii = 1:numel(X)
-    for i = cvset
-      for j = 1:nalpha
-        if isempty(ALPHA)
-          alpha = nan(1);
-        elseif length(ALPHA) > 1
-          alpha = ALPHA(j);
-        else
-          alpha = ALPHA;
-        end
-        for k = 1:nlam
-          if isempty(LAMBDA)
-            lambda = nan(1);
-          elseif length(LAMBDA) > 1
-            lambda = LAMBDA(k);
-          else
-            lambda = LAMBDA;
-          end
-
+        for ii = 1:numel(X);
           iii = iii + 1;
 
-          wz = WzAll{ii}{i,j,k};
+          wz = Wz{ii}
           ix = find(wz);
           nv = numel(wz);
           wnz = nnz(wz);
           wz = wz(ix);
+
+          y = Y{ii};
+          yz = Yz{ii};
+          h1   = nnz( y(test_set{ii})>0  & (yz(test_set{ii})>0)  );
+          h2   = nnz( y(train_set{ii})>0 & (yz(train_set{ii})>0) );
+          f1   = nnz(~y(test_set{ii})>0  & (yz(test_set{ii})>0)  );
+          f2   = nnz(~y(train_set{ii})>0 & (yz(train_set{ii})>0) );
+          err1 = nnz( y(test_set{ii})>0  ~= (yz(test_set{ii})>0 ));
+          err2 = nnz( y(train_set{ii})>0 ~= (yz(train_set{ii})>0));
+          nt1  = nnz(y(test_set{ii})>0);
+          nt2  = nnz(y(train_set{ii})>0);
+          nd1  = nnz(y(test_set{ii})<=0);
+          nd2  = nnz(y(train_set{ii})<=0);
+
           if ~SMALL
             results(iii).Wz = wz;
             results(iii).Wix = uint32(ix);
-            results(iii).Yz = YzAll{ii}{i,j,k};
+            results(iii).Yz = Yz{ii};
           end
+
           results(iii).Wnz = uint32(wnz);
           results(iii).nvox = uint32(nv);
-
           results(iii).subject = uint8(ii);
-          results(iii).cvholdout = uint8(i);
-          results(iii).finalholdout = uint8(0);
+          results(iii).cvholdout = uint8(icv);
+          results(iii).finalholdout = uint8(0); % handled in parent function
           results(iii).alpha = alpha;
           results(iii).lambda = lambda;
           results(iii).diameter = 0;
           results(iii).overlap = 0;
           results(iii).shape = '';
-          results(iii).nt1  = uint16(nt1{ii}(i,j,k));
-          results(iii).nt2  = uint16(nt2{ii}(i,j,k));
-          results(iii).nd1  = uint16(nd1{ii}(i,j,k));
-          results(iii).nd2  = uint16(nd2{ii}(i,j,k));
-          results(iii).h1   = uint16(h1{ii}(i,j,k));
-          results(iii).h2   = uint16(h2{ii}(i,j,k));
-          results(iii).f1   = uint16(f1{ii}(i,j,k));
-          results(iii).f2   = uint16(f2{ii}(i,j,k));
-          results(iii).err1 = uint16(err1{ii}(i,j,k));
-          results(iii).err2 = uint16(err2{ii}(i,j,k));
+          results(iii).nt1  = uint16(nt1);
+          results(iii).nt2  = uint16(nt2);
+          results(iii).nd1  = uint16(nd1);
+          results(iii).nd2  = uint16(nd2);
+          results(iii).h1   = uint16(h1);
+          results(iii).h2   = uint16(h2);
+          results(iii).f1   = uint16(f1);
+          results(iii).f2   = uint16(f2);
+          results(iii).err1 = uint16(err1);
+          results(iii).err2 = uint16(err2);
+
+          fprintf('cv %3d: %3d |%6.2f |%6.2f |%10d |%10d |%10d |%10d |%10d |\n', ...
+            i,ii,alpha_j,lambda_k,err1,err2,h1-f1,h2-f2,wnz);
         end
-      end
-    end
-  end
+      end % lamba loop
+    end % alpha loop
+	end % cv loop
   fprintf('logged %d results in memory.\n', iii);
 end
