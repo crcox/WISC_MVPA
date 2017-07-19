5b08905a01da5c36b0145e6329d0c7f2cb088487
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2017-07-19 19:08:39 +0100

    stupid mistake...

--- src/init_glmnet_struct.m
+++ src/init_glmnet_struct.m
@@ -7,5 +7,5 @@ function y = init_glmnet_struct(opts)
     x = glmnet(magic(10),repmat([0;1],5,1),'binomial',opts);
     f = fieldnames(x);
     x = cell2struct(repmat({[]}, numel(f), 1), f);
-    y = repmat(x, dims);
+    y = repmat(x, 1); % fix later maybe?
 end
