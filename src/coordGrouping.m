fa490cf3e449c507a04f664c68d7839e54dff76c
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-04-20 18:01:10 +0100

    Revised coordinate grouping

--- src/coordGrouping.m
+++ src/coordGrouping.m
@@ -21,128 +21,108 @@ function G = coordGrouping(coords, diameter, overlap, shape)
     %       indexes that point to rows in the corresponding cell of coords.
     %
     %   See also ndmovingwindow, ndcoord.
-    %   Author: Chris Cox, 7/8/2015
-    % Older releases of matlab do not have the repelem() builtin. If that is the case, use my crappy version instead.
-    mlinfo = ver('MATLAB');
-    mlversion = sscanf(mlinfo.Version,'%d.%d');
-    if (mlversion(1) <= 8) && (mlversion(2) < 5)
-        repelem_ = @repelem_crc;
-    else
-        repelem_ = @repelem;
-    end
+    %   Author: Chris Cox, 20 April 2018
 
     p = inputParser();
-    addRequired(p, 'coords',   @isnumericlike);
+    addRequired(p, 'coords',   @(x) iscell(x) && isnumeric(x{1}));
     addRequired(p, 'diameter', @isnumeric);
     addRequired(p, 'overlap',  @isnumeric);
-    addRequired(p, 'shape',    @isvalidshape);
+    addRequired(p, 'shape',    @(x) any(strcmpi(x,{'cube','sphere','unitary'})));
     parse(p, coords, diameter, overlap, shape);
 
     coords   = p.Results.coords;
     diameter = p.Results.diameter(:)';
     overlap  = p.Results.overlap(:)';
     shape    = p.Results.shape;
-    if iscell(coords)
-        d = cellfun(@(x) size(x,2), coords);
-        n = cellfun(@(x) size(x,1), coords);
-        n = n(:)'; % force row vec
-        N = length(coords(:));
-        assert(all(d(1) == d));
-        coords = cell2mat(coords(:));
-        sid = repelem_(1:N, n);
-        ncum = uint32([0, cumsum(n)]);
-    else
-        [n,d] = size(coords);
-        N = 1;
-    end
-
+    
+    d = cellfun(@(x) size(x,2), coords);
+    n = cellfun(@(x) size(x,1), coords);
+    n = n(:)'; % force row vec
+    N = length(coords(:));
+    assert(all(d(1) == d));
+    ncum = uint32([0, cumsum(n)]);
+    
     assert(length(diameter) == 1 || all(length(diameter) == d));
     assert(length(overlap) == 1 || all(length(overlap) == d));
     assert(all(overlap <= diameter));
 
+    d = d(1);
+    if numel(diameter) == 1
+        diameter = repmat(diameter, d, 1);
+    end
+    
+    x = (diameter - overlap);% ./ 2;
     r = diameter ./ 2;
-    x = (diameter - overlap) ./ 2;
 
-    radii = true(size(coords,1),1);
-    G = cell(size(coords,1),N);
-    if any(r == 0)
-        if N > 1
-            for j = 1:N
-                a = ncum(j) + 1;
-                b = ncum(j+1);
-                G(a:b, j) = num2cell(uint32(1:n(j)));
-                G(a:b, j~=[1:N]) = {uint32(zeros(0,1))};
+    extents.min = floor(min(cell2mat(coords)));
+    extents.max = ceil(max(cell2mat(coords)));
+    g = cell(d,1);
+    for i = 1:d
+        a = extents.min(i);
+        b = extents.max(i);
+        % g{i} will contain the onset of each group in a particular
+        % dimension.
+        g{i} = a:x(i):b;
+        if strcmpi(shape, 'sphere')
+            % If we are generating spheres, the points we are ultimately
+            % generating are going to be the radii. If this point is
+            % further than r(i) away from b, then we should add an
+            % additional point to compose an additional radii.
+            if (g{i}(end) + r(i)) < b
+                g{i} = [g{i},g{i}(end)+x(i)];
             end
-        else
-            G(:, 1) = num2cell(uint32(1:n(1)));
         end
+    end
+    gg = cell(d,1);
+    [gg{:}] = ndgrid(g{:});
+    gg = cellfun(@(x) x(:), gg, 'UniformOutput', 0);
+    gg = cat(2,gg{:});
+    
+    G = cell(size(gg,1), N);
+    switch lower(shape)
+        case 'sphere'
 
-    else
-        for i = 1:size(coords,1);
-            if radii(i) == true;
-                switch lower(shape)
-                    case 'sphere'
-                        tmp = bsxfun(@minus,coords, coords(i,:)).^2;
-                        z = sum(bsxfun(@rdivide, tmp, r.^2),2) < 1;
-                        g = uint32(find(z));
-                        if N > 1
-                            s = sid(z);
-                            for j = 1:N
-                                G{i,j} = g(s==j) - ncum(j);
-                            end
-                        else
-                            G{i,1} = g;
-                        end
-                        z = sum(bsxfun(@rdivide, tmp, x.^2),2) < 1;
-                        radii(z) = false;
-                    case 'cube'
-                        cmax = coords(i,:) + r;
-                        cmin = coords(i,:) - r;
-%                         z = all(bsxfun(@le, abs(bsxfun(@minus,coords,coords(i,:))), r),2);
-                        
-                        z = all(bsxfun(@lt,coords,cmax) & bsxfun(@ge,coords,cmin),2);
-                        g = uint32(find(z));
-                        if N > 1
-                            s = sid(z);
-                            for j = 1:N
-                                G{i,j} = g(s==j) - ncum(j);
-                            end
-                        else
-                            G{i,1} = g;
-                        end
-                        cmax = coords(i,:) + x;
-                        cmin = coords(i,:) - x;
-                        z = all(bsxfun(@lt,coords,cmax) & bsxfun(@ge,coords,cmin),2);
-                        radii(z) = false;
-                    case 'unitary'
-                        warning('coordGrouping:ignoringArgs','The unitary shape implies diameter = overlap = 0. Nonzero values provided are being ignored.');
-                        if N > 1
-                            for j = 1:N
-                                a = ncum(j) + 1;
-                                b = ncum(j+1);
-                                G(a:b, j) = num2cell(uint32(1:n(j)));
-                                G(a:b, j~=[1:N]) = {uint32(zeros(0,1))};
-                            end
-                        else
-                            G(:, 1) = num2cell(uint32(1:n(1)));
-                        end
-                        break
-
+            for j = 1:N
+                for i = 1:size(gg,1)
+                    di = distance_to_reference(gg(i,:), coords{j});
+                    z = all(bsxfun(@le,di(:), r(:)'), 2);
+                    G{i,j} = uint32(find(z));        
                 end
             end
-        end
+
+        case 'cube'
+            for j = 1:N
+                gd = bsxfun(@plus, gg, diameter(:)');
+                for i = 1:size(gg,1)
+                    z = bsxfun(@ge, coords{j}, gg(i,:)) & bsxfun(@lt, coords{j}, gd(i,:));
+                    z = all(z, 2);
+                    G{i,j} = uint32(find(z));        
+                end
+            end
+            
+        case 'unitary'
+            warning('coordGrouping:ignoringArgs','The unitary shape implies diameter = overlap = 0. Nonzero values provided are being ignored.');
+            if N > 1
+                for j = 1:N
+                    a = ncum(j) + 1;
+                    b = ncum(j+1);
+                    G(a:b, j) = num2cell(uint32(1:n(j)));
+                    G(a:b, j~=(1:N)) = {uint32(zeros(0,1))};
+                end
+            else
+                G(:, 1) = num2cell(uint32(1:n(1)));
+            end
     end
     G(all(cellfun('isempty',G),2),:) = [];
 end
 
-function b = isvalidshape(x)
-    b = ischar(x) && any(strcmpi(x,{'sphere','cube','unitary'}));
-end
-
-function b = isnumericlike(x)
-    if iscell(x)
-        b = all(cellfun(@isnumeric, x));
-    else
-        b = isnumeric(x);
+function d = distance_to_reference(reference, points)
+    z = zeros(size(points,1),1);
+    for i = 1:numel(reference)
+        xp=reference(i);
+        x = points(:,i);
+        y = ((xp-x).^2);
+        z = z + y;
     end
+    d = sqrt(z);
 end
