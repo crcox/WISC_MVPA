bd6ee2948a9dc0ca3ccf06ace191878878e3340a
Chris Cox <crcox@wisc.edu>
2018-07-21 16:29:33 -0500

    Allow OSG module init script to proceed even if there are errors

--- run_WISC_MVPA.sh
+++ run_WISC_MVPA.sh
@@ -106,7 +106,10 @@ done < ALLURLS
 
 # Run the Matlab application
 if [ $isOSG = "True" ]; then
+  # This script needs to run to the end, even if there are errors.
+  set +e
   source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/current/init/bash
+  set -e
   module load matlab/2015b
 
 else
