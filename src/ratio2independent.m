dd59b3a8b1b2916da5edfd3b53a07fb6afe96319
slfrisby <saskia.frisby@mrc-cbu.cam.ac.uk>
2024-07-08 17:02:28 +0100

    fixed error with matrix multiplication

--- src/ratio2independent.m
+++ src/ratio2independent.m
@@ -1,4 +1,4 @@
 function [lamSOS,lamL1] = ratio2independent(alpha, lambda)
-    lamL1 = lambda * (1-alpha);
-    lamSOS = lambda * alpha;
+    lamL1 = lambda .* (1-alpha);
+    lamSOS = lambda .* alpha;
 end
